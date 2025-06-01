a484edeea7eef9b587c411d9bb493858f292058f
Vu Quoc Tuan <tuan@scaleflex.com>
2025-06-01 21:27:30 +0700

    update code

--- lesson05-exercise-user-manager/cmd/api/main.go
+++ lesson05-exercise-user-manager/cmd/api/main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
+	"user-management-api/internal/app"
 	"user-management-api/internal/config"
-	"user-management-api/internal/handler"
-	"user-management-api/internal/repository"
-	"user-management-api/internal/routes"
-	"user-management-api/internal/service"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Initialize configuration
 	cfg := config.NewConfig()
 
-	// Initialize repository
-	userRepo := repository.NewInMemoryUserRepository()
+	// Initialize application
+	application := app.NewApplication(cfg)
 
-	// Initialize service
-	userService := service.NewUserService(userRepo)
-
-	// Initialize handler
-	userHandler := handler.NewUserHandler(userService)
-
-	// Initialize routes
-	userRoutes := routes.NewUserRoutes(userHandler)
-
-	r := gin.Default()
-
-	routes.RegisterRoutes(r, userRoutes)
-
-	if err := r.Run(cfg.ServerAddress); err != nil {
+	// Start server
+	if err := application.Run(); err != nil {
 		panic(err)
 	}
 }

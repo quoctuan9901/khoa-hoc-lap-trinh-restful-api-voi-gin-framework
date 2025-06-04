8d2a76d1c9b161b4c91176d972945a27a5b3f4ef
Vu Quoc Tuan <tuan@scaleflex.com>
2025-06-04 15:32:35 +0700

    update code

--- lesson05-exercise-user-manager/internal/service/interfaces.go
+++ lesson05-exercise-user-manager/internal/service/interfaces.go
@@ -3,7 +3,7 @@ package service
 import "user-management-api/internal/models"
 
 type UserService interface {
-	GetAllUsers() ([]models.User, error)
+	GetAllUsers(search string, page, limit int) ([]models.User, error)
 	CreateUser(user models.User) (models.User, error)
 	GetUserByUUID(uuid string) (models.User, error)
 	UpdateUser()
--- lesson05-exercise-user-manager/internal/service/user_service.go
+++ lesson05-exercise-user-manager/internal/service/user_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"strings"
 	"user-management-api/internal/models"
 	"user-management-api/internal/repository"
 	"user-management-api/internal/utils"
@@ -19,13 +20,38 @@ func NewUserService(repo repository.UserRepository) UserService {
 	}
 }
 
-func (us *userService) GetAllUsers() ([]models.User, error) {
+func (us *userService) GetAllUsers(search string, page, limit int) ([]models.User, error) {
 	users, err := us.repo.FindAll()
 	if err != nil {
 		return nil, utils.WrapError(err, "failed to fetch users", utils.ErrCodeInternal)
 	}
 
-	return users, nil
+	var filteredUsers []models.User
+	if search != "" {
+		search = strings.ToLower(search)
+		for _, user := range users {
+			name := strings.ToLower(user.Name)
+			email := strings.ToLower(user.Email)
+
+			if strings.Contains(name, search) || strings.Contains(email, search) {
+				filteredUsers = append(filteredUsers, user)
+			}
+		}
+	} else {
+		filteredUsers = users
+	}
+
+	start := (page - 1) * limit
+	if  start >= len(filteredUsers) {
+		return []models.User{}, nil
+	}
+
+	end := start + limit
+	if end > len(filteredUsers) {
+		end = len(filteredUsers)
+	}
+
+	return filteredUsers[start:end], nil
 }
 
 func (us *userService) CreateUser(user models.User) (models.User, error) {

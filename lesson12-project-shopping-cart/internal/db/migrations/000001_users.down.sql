af71330b39598dbde020584876a88e4e3f7bd06d
Vu Quoc Tuan <tuan@scaleflex.com>
2025-06-19 23:27:47 +0700

    update code

--- lesson12-project-shopping-cart/internal/db/migrations/000001_users.down.sql
+++ lesson12-project-shopping-cart/internal/db/migrations/000001_users.down.sql
@@ -1,1 +1,15 @@
-drop table if exists users;
+-- Drop trigger if exist
+DROP TRIGGER IF EXISTS set_user_updated_at on users;
+
+-- Drop tigger function
+DROP FUNCTION IF EXISTS update_user_updated_at_column;
+
+-- Drop index
+DROP INDEX IF EXISTS idx_users_status;
+DROP INDEX IF EXISTS idx_users_level;
+DROP INDEX IF EXISTS idx_users_created_at;
+DROP INDEX IF EXISTS idx_user_deleted_at;
+DROP INDEX IF EXISTS idx_users_email_status;
+
+-- Drop table
+DROP TABLE IF EXISTS users;
